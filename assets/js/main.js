62f57e371ade445ed90e9312d1c7c4e6a47248c6
Daniel Gomez <gomez.danielg@gmail.com>
2017-10-31 16:43:19 -0300

    First commit.

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+var Vue = require('vue');
+//var VueCarousel = require('vue-carousel');
+
+//Vue.use(VueCarousel);
+
+var app = new Vue({
+  el: '#app',
+  data: {
+    message: 'Hello Vue!'
+  }
+});
---
+++ server.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const express = require('express');
+const path = require('path');
+
+// Constants
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+// App
+const app = express();
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname + '/views/index.html'));
+});
+
+app.use(express.static('public'));
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
